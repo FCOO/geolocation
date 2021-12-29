1c04a5a029b123a06be0a4f8d17d1a38cfec168f
Niels Holt <niels@steenbuchholt.dk>
2021-12-29 17:24:30 +0100

    No message

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 
 
 ## Description
-Events, Provider and Handler of geolocations
+Contains tree parts regarding geolocation and device orientation
+Events for device orientation
+Classes for geolocation-provider and -handler
+geolocation-provider for standard geolocation API
 
 
 ## Installation
@@ -14,19 +17,46 @@ Events, Provider and Handler of geolocations
 http://FCOO.github.io/geolocation/demo/
 
 ## Usage
-```var myGeolocation = new Geolocation( options );```
+
+### Device orientation events
+There are two events regarding device orientation
+
+- [deviceorientation](https://developer.mozilla.org/en-US/docs/Web/Events/Detecting_device_orientation)
+- [compassneedscalibration](https://www.w3.org/TR/orientation-event/#compassneedscalibration)
+
+The different browser do not handle this events the same way. Therefor there are methods to add and remove event-handlers for this events.
+Safari on iOS doesn't implement the spec correctly, because alpha is arbitrary instead of relative to true north.
+Safari instead offers `webkitCompassHeading`, which has the opposite sign to alpha and is also relative to magnetic north instead of true north.
+`DeviceOrientationEvent.beta` has values between -90 and 90 on mobile Safari and between 180 and -180 on Firefox.
+`DeviceOrientationEvent.gamma` has values between -180 and 180 on mobile Safari and between 90 and -90 on Firefox.
+
+Se details for 
+[Firefox, Chrome](https://developer.mozilla.org/en-US/docs/Web/API/DeviceOrientationEvent) and 
+[Safari](https://developer.apple.com/documentation/webkitjs/deviceorientationevent#//apple_ref/javascript/instp/DeviceOrientationEvent/beta)
 
 
-### options
-| Id | Type | Default | Description |
-| :--: | :--: | :-----: | --- |
-| options1 | boolean | true | If <code>true</code> the ... |
-| options2 | string | null | Contain the ... |
 
-### Methods
+     To add events-handler to these events use
+        window.geolocation.onDeviceorientation( fn: FUNCTION(EVENT)[, context: OBJECT] )
+        window.geolocation.onCompassneedscalibration( fn: FUNCTION(EVENT)[, context: OBJECT] )
 
-    .methods1( arg1, arg2,...): Do something
-    .methods2( arg1, arg2,...): Do something else
+    To remove events-handler to these events use
+        window.geolocation.offDeviceorientation( fn: FUNCTION[, context: OBJECT] )
+        window.geolocation.offCompassneedscalibration( fn: FUNCTION[, context: OBJECT] )
+
+    EVENT = {
+        absolute: BOOLEAN,
+        deviceorientation   : NUMBER or null,   //Calculated
+        webkitCompassHeading: NUMBER or null,
+        alpha               : NUMBER or null,
+        beta                : NUMBER or null,
+        gamma               : NUMBER or null,
+    }
+
+See [description of EVENT elements](https://developer.mozilla.org/en-US/docs/Web/API/DeviceOrientationEvent)
+
+### Geolocation-
+
 
 
 
@@ -38,6 +68,3 @@ Copyright (c) 2021 [FCOO](https://github.com/FCOO)
 ## Contact information
 
 Niels Holt nho@fcoo.dk
-
-
-## Credits and acknowledgements
--- src/geolocation-standard.js
+++ src/geolocation-standard.js
@@ -1,6 +1,8 @@
 /****************************************************************************
 geolocation-standard.js
 
+Creates window.geolocation.provider = version of GeolocationProvider that
+provides location from the browser geolocation API
 
 ****************************************************************************/
 
@@ -11,33 +13,34 @@ geolocation-standard.js
     var ns = window.geolocation = window.geolocation || {};
 
     var geolocationOptions = {
-        /* From https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+        /* From https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition */
+
+        /*
         maximumAge:
             Is a positive long value indicating the maximum age in milliseconds of a possible cached position that is acceptable to return.
             If set to 0, it means that the device cannot use a cached position and must attempt to retrieve the real current position.
             If set to Infinity the device must return a cached position regardless of its age. Default: 0.
+        */
+        //maximumAge          : 10 * 1000, //Allow 10sec old position
 
+        /*
         timeout:
             Is a positive long value representing the maximum length of time (in milliseconds) the device is allowed to take in order to return a position.
             The default value is Infinity, meaning that getCurrentPosition() won't return until the position is available.
+        */
+        //timeout             : 10 * 1000,
 
+        /*
         enableHighAccuracy:
             Is a boolean value that indicates the application would like to receive the best possible results.
             If true and if the device is able to provide a more accurate position, it will do so.
             Note that this can result in slower response times or increased power consumption (with a GPS chip on a mobile device for example).
             On the other hand, if false, the device can take the liberty to save resources by responding more quickly and/or using less power. Default: false.
         */
-
-        maximumAge          : 10 * 1000, //Allow 10sec old position
-
-        timeout             : 10 * 1000,
-
-        enableHighAccuracy  : false
+        enableHighAccuracy  : true
     };
 
     /*
-
-
     GeolocationPosition = {
         coords   : GeolocationCoordinates
         timestamp: Millisecond
@@ -53,8 +56,42 @@ geolocation-standard.js
         speed           : Velocity of the device in meters per second. This value can be null.
     }
     All properties are double
-
     */
+
+
+    var Provider = function(){
+            this.lastCoords = {};
+            ns.GeolocationProvider.call(this, geolocationOptions);
+        }
+
+    Provider.prototype = Object.create(ns.GeolocationProvider.prototype);
+
+
+    $.extend(Provider.prototype, {
+        activate: function(){
+            this._success = this._success || $.proxy(this.success, this);
+            this._error   = this._error   || $.proxy(this.onError, this);
+
+            if (navigator.geolocation)
+                navigator.geolocation.watchPosition(this._success, this._error, geolocationOptions);
+            else
+                this.onError(4);
+        },
+
+
+        deactivate: function(){
+            if (navigator.geolocation)
+                navigator.clearWatch();
+        },
+
+        success: function( GeolocationPosition ){
+            this.update( GeolocationPosition.coords );
+        },
+
+
+
+
+
 /*
 var timestamp = 0;
     function niels( geolocationPosition ){
@@ -112,6 +149,10 @@ console.log(geolocationPosition);
 //HER        }
 //HER    });
 
+    });
+
+    //Create window.geolocation.provider
+    ns.provider = new Provider();
 
 }(jQuery, this/*, document*/));
 
